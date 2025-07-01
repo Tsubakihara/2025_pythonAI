06b799b2db33a49edd4cb4823e62746ed2a90be4
Tsubakihara <65284339+Tsubakihara@users.noreply.github.com>
2025-07-01 10:42:48 +0900

    Update q5_enter_comment.py

--- 20250624/q5_enter_comment.py
+++ 20250624/q5_enter_comment.py
@@ -1,7 +1,7 @@
 # カスタマーサポート対応後の業務記録として、対応内容をメモ形式で記録してください。
 # 複数行の入力欄を表示し、記入内容をファイル出力用に整形して画面出力します。
 
-import PySimpleGUI as sg
+import FreeSimpleGUI as sg
 
 def input_support_memo():
     memo = sg.XXXXXXXXX("顧客対応内容を入力してください（200文字以内）", "対応記録入力", [60, 10])
