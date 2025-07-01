c2a296c9ddcb5536d9ced4b3c0d671d8fa7b3be8
Tsubakihara <65284339+Tsubakihara@users.noreply.github.com>
2025-07-01 10:42:34 +0900

    Update q4_select_delivery_date.py

--- 20250624/q4_select_delivery_date.py
+++ 20250624/q4_select_delivery_date.py
@@ -2,7 +2,7 @@
 # 日付は選択式ダイアログで入力できるようにしてください。
 #  選択された日付は確認メッセージとして表示されます。
 
-import PySimpleGUI as sg
+import FreeSimpleGUI as sg
 
 def register_delivery_date():
     date = sg.XXXXXXXXX("出荷予定日を選択してください", "出荷日登録")
